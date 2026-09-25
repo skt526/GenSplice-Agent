e0185842b749220e4f0344248bd7beb9f9b572c5
Kitae Song <kitae@MacBook-Air-4.local>
2026-09-25 20:49:30 +0900

    Configure test.sh downloader for GSE52778 N=3 Control vs N=3 Dexamethasone biological replicates

--- test.sh
+++ test.sh
@@ -67,21 +67,40 @@ for candidate in \
 done
 
 if [ "$MODE" = "--real" ] || [ "$MODE" = "real" ]; then
-    echo -e "${BOLD}[1/4] Preparing REAL SRA Data Test Mode (SRR1039508 & SRR1039512)...${RESET}"
+    echo -e "${BOLD}[1/4] Preparing REAL SRA Data Test Mode (GSE52778: N=3 Control vs N=3 Dexamethasone)...${RESET}"
     
     mkdir -p inputs/control inputs/treatment
     
-    CTRL_R1="inputs/control/GSM1275862_SRR1039508_1.fq.gz"
-    CTRL_R2="inputs/control/GSM1275862_SRR1039508_2.fq.gz"
-    TREAT_R1="inputs/treatment/GSM1275866_SRR1039512_1.fq.gz"
-    TREAT_R2="inputs/treatment/GSM1275866_SRR1039512_2.fq.gz"
+    # GSE52778 Biological Replicates: 3 Control (SRR1039508, SRR1039512, SRR1039516) vs 3 Dex Treated (SRR1039509, SRR1039513, SRR1039517)
+    CTRL1_R1="inputs/control/GSM1275862_SRR1039508_1.fq.gz"
+    CTRL1_R2="inputs/control/GSM1275862_SRR1039508_2.fq.gz"
+    CTRL2_R1="inputs/control/GSM1275866_SRR1039512_1.fq.gz"
+    CTRL2_R2="inputs/control/GSM1275866_SRR1039512_2.fq.gz"
+    CTRL3_R1="inputs/control/GSM1275870_SRR1039516_1.fq.gz"
+    CTRL3_R2="inputs/control/GSM1275870_SRR1039516_2.fq.gz"
 
-    URL_CTRL_R1="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/008/SRR1039508/SRR1039508_1.fastq.gz"
-    URL_CTRL_R2="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/008/SRR1039508/SRR1039508_2.fastq.gz"
-    URL_TREAT_R1="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/002/SRR1039512/SRR1039512_1.fastq.gz"
-    URL_TREAT_R2="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/002/SRR1039512/SRR1039512_2.fastq.gz"
+    TREAT1_R1="inputs/treatment/GSM1275863_SRR1039509_1.fq.gz"
+    TREAT1_R2="inputs/treatment/GSM1275863_SRR1039509_2.fq.gz"
+    TREAT2_R1="inputs/treatment/GSM1275867_SRR1039513_1.fq.gz"
+    TREAT2_R2="inputs/treatment/GSM1275867_SRR1039513_2.fq.gz"
+    TREAT3_R1="inputs/treatment/GSM1275871_SRR1039517_1.fq.gz"
+    TREAT3_R2="inputs/treatment/GSM1275871_SRR1039517_2.fq.gz"
 
-    echo -e "\n${BOLD}Downloading Real FASTQ Data from ENA/NCBI SRA...${RESET}"
+    URL_CTRL1_R1="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/008/SRR1039508/SRR1039508_1.fastq.gz"
+    URL_CTRL1_R2="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/008/SRR1039508/SRR1039508_2.fastq.gz"
+    URL_CTRL2_R1="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/002/SRR1039512/SRR1039512_1.fastq.gz"
+    URL_CTRL2_R2="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/002/SRR1039512/SRR1039512_2.fastq.gz"
+    URL_CTRL3_R1="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/006/SRR1039516/SRR1039516_1.fastq.gz"
+    URL_CTRL3_R2="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/006/SRR1039516/SRR1039516_2.fastq.gz"
+
+    URL_TREAT1_R1="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/009/SRR1039509/SRR1039509_1.fastq.gz"
+    URL_TREAT1_R2="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/009/SRR1039509/SRR1039509_2.fastq.gz"
+    URL_TREAT2_R1="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/003/SRR1039513/SRR1039513_1.fastq.gz"
+    URL_TREAT2_R2="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/003/SRR1039513/SRR1039513_2.fastq.gz"
+    URL_TREAT3_R1="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/007/SRR1039517/SRR1039517_1.fastq.gz"
+    URL_TREAT3_R2="https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/007/SRR1039517/SRR1039517_2.fastq.gz"
+
+    echo -e "\n${BOLD}Downloading Real FASTQ Data (N=3 Control vs N=3 Dex Treated) from ENA/NCBI SRA...${RESET}"
     
     find_existing_fastq() {
         local dir="$1"
@@ -112,10 +131,21 @@ if [ "$MODE" = "--real" ] || [ "$MODE" = "real" ]; then
         fi
     }
 
-    download_if_missing "$CTRL_R1" "$URL_CTRL_R1" "Control Read 1 (SRR1039508_1)" "SRR1039508_1"
-    download_if_missing "$CTRL_R2" "$URL_CTRL_R2" "Control Read 2 (SRR1039508_2)" "SRR1039508_2"
-    download_if_missing "$TREAT_R1" "$URL_TREAT_R1" "Treatment Read 1 (SRR1039512_1)" "SRR1039512_1"
-    download_if_missing "$TREAT_R2" "$URL_TREAT_R2" "Treatment Read 2 (SRR1039512_2)" "SRR1039512_2"
+    # Control Replicates (N=3)
+    download_if_missing "$CTRL1_R1" "$URL_CTRL1_R1" "Control 1 Read 1 (SRR1039508_1)" "SRR1039508_1"
+    download_if_missing "$CTRL1_R2" "$URL_CTRL1_R2" "Control 1 Read 2 (SRR1039508_2)" "SRR1039508_2"
+    download_if_missing "$CTRL2_R1" "$URL_CTRL2_R1" "Control 2 Read 1 (SRR1039512_1)" "SRR1039512_1"
+    download_if_missing "$CTRL2_R2" "$URL_CTRL2_R2" "Control 2 Read 2 (SRR1039512_2)" "SRR1039512_2"
+    download_if_missing "$CTRL3_R1" "$URL_CTRL3_R1" "Control 3 Read 1 (SRR1039516_1)" "SRR1039516_1"
+    download_if_missing "$CTRL3_R2" "$URL_CTRL3_R2" "Control 3 Read 2 (SRR1039516_2)" "SRR1039516_2"
+
+    # Treatment Replicates (N=3)
+    download_if_missing "$TREAT1_R1" "$URL_TREAT1_R1" "Treatment 1 Read 1 (SRR1039509_1)" "SRR1039509_1"
+    download_if_missing "$TREAT1_R2" "$URL_TREAT1_R2" "Treatment 1 Read 2 (SRR1039509_2)" "SRR1039509_2"
+    download_if_missing "$TREAT2_R1" "$URL_TREAT2_R1" "Treatment 2 Read 1 (SRR1039513_1)" "SRR1039513_1"
+    download_if_missing "$TREAT2_R2" "$URL_TREAT2_R2" "Treatment 2 Read 2 (SRR1039513_2)" "SRR1039513_2"
+    download_if_missing "$TREAT3_R1" "$URL_TREAT3_R1" "Treatment 3 Read 1 (SRR1039517_1)" "SRR1039517_1"
+    download_if_missing "$TREAT3_R2" "$URL_TREAT3_R2" "Treatment 3 Read 2 (SRR1039517_2)" "SRR1039517_2"
 
     # Ensure Reference Genome is ready
     if [ ! -d "human-ref" ] || [ ! -f "human-ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa" ]; then
